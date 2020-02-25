db3b7f3cd280ce6995ef917b9a11c7c67b02c49b
katieliebscher <katieliebscher@gmail.com>
2020-02-25 15:16:48 -0500

    New File Split Up, defense more effective but still needs work, began integrating path prediction

---
+++ CarstianoRevnaldbot-master/src/Debug.py
@@ -0,0 +1,13 @@
+def draw_debug(renderer, car, ball, action_display):
+    renderer.begin_rendering()
+    # draw a line from the car to the ball
+    renderer.draw_line_3d(car.physics.location, ball.physics.location, renderer.white())
+    # print the action that the bot is taking
+    renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
+    renderer.end_rendering()
+
+def draw_debug_message(renderer, car, action_display):
+    renderer.begin_rendering()
+    # print the action that the bot is taking
+    renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
+    renderer.end_rendering()
