cd64a69611624caa00deeb3c2cb83f53e652f5cf
Angel Rodriguez <34219974+rodriang000@users.noreply.github.com>
2020-02-26 02:32:19 -0500

    Add files via upload

--- CarstianoRevnaldbot-master/src/Actions.py
+++ CarstianoRevnaldbot-master/src/Actions.py
@@ -43,6 +43,11 @@ def drive_for_kickoff(self, my_car, car_to_desired_location) -> str:
     self.controller_state.throttle = 1.0
     self.controller_state.steer = turn 
     self.controller_state.boost = True
+
+    car_location = Vec3(my_car.physics.location)
+    if (car_location.dist(car_to_desired_location) < 900):
+        jump(self)
+
     return action_display  
 
 def drive_toward_ball(self, my_car, car_to_ball):
@@ -79,8 +84,8 @@ def drive_toward(self, my_car, car_to_desired_location) -> str:
         action_display = "drive straight"
     
     self.controller_state.throttle = 1.0
-    self.controller_state.steer = turn   
-    return action_display  
+    self.controller_state.steer = turn
+    return action_display
 
 def find_correction(current: Vec3, ideal: Vec3, yaw) -> float:
     # Finds the angle from current to ideal vector in the xy-plane. Angle will be between -pi and +pi.
@@ -116,6 +121,10 @@ def inside_turning_radius(my_car, car_to_desired_location) -> bool:
     turn_radius = turning_radius(car_speed)
     return turn_radius > min((local - Vec3(0, -turn_radius, 0)).length(), (local - Vec3(0, turn_radius, 0)).length())
 
+# Jumping (And eventually, hopefully, air movement)
+
+def jump(self):
+    self.controller_state.jump = True
 
 # Braking
 def brake_by(self, my_car, location):
