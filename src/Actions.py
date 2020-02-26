fde7bae347fb894552b206336833e20891fa326a
Angel Rodriguez <34219974+rodriang000@users.noreply.github.com>
2020-02-26 02:30:30 -0500

    Add files via upload

---
+++ src/Actions.py
@@ -0,0 +1,190 @@
+import math
+
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+
+from util.orientation import Orientation
+from util.vec import Vec3
+
+from Debug import *
+from Calculations import *
+from util.Matrix import *
+
+
+# TODO calculate turning radius and turning radius needed, if you can't make the turn then brake
+# Path Prediction
+
+def get_ball_predicted_pos(self, sliceNum) -> Vec3:
+    ball_prediction = self.get_ball_prediction_struct()
+    return Vec3(ball_prediction.slices[sliceNum].physics.location)
+
+   
+# General Driving
+def drive_for_kickoff(self, my_car, car_to_desired_location) -> str:
+    # Find the direction of our car using the Orientation class
+    car_orientation = Orientation(my_car.physics.rotation)
+    car_direction = car_orientation.forward
+    car_yaw = my_car.physics.rotation.yaw
+    steer_correction_radians = find_correction(car_direction, car_to_desired_location, car_yaw)
+
+    if steer_correction_radians < math.radians(-10):
+        # If the target is more than 10 degrees right from the centre, steer left
+        turn = -1
+        action_display = "turn left"
+    elif steer_correction_radians > math.radians(10):
+        # If the target is more than 10 degrees left from the centre, steer right
+        turn = 1
+        action_display = "turn right"
+    else:
+        # If the target is less than 10 degrees from the centre, steer straight
+        turn = 0
+        action_display = "drive straight"
+        
+    self.controller_state.throttle = 1.0
+    self.controller_state.steer = turn 
+    self.controller_state.boost = True
+
+    car_location = Vec3(my_car.physics.location)
+    if (car_location.dist(car_to_desired_location) < 900):
+        jump(self)
+
+    return action_display  
+
+def drive_toward_ball(self, my_car, car_to_ball):
+    drive_toward(self, my_car, car_to_ball)
+    
+def calculate_ball_to_goal(ball_location, goal)-> float:
+    return ball_location.dist(goal.location)
+
+def get_car_boost_level(my_car) -> float:
+    return my_car.boost
+
+def drive_toward_boost(self, my_car, car_to_boost):
+    drive_toward(self, my_car, car_to_boost)
+
+def drive_toward(self, my_car, car_to_desired_location) -> str:
+    self.controller_state.boost = False
+     # Find the direction of our car using the Orientation class
+    car_orientation = Orientation(my_car.physics.rotation)
+    car_direction = car_orientation.forward
+    car_yaw = my_car.physics.rotation.yaw
+    steer_correction_radians = find_correction(car_direction, car_to_desired_location, car_yaw)
+
+    if steer_correction_radians < math.radians(-10):
+        # If the target is more than 10 degrees right from the centre, steer left
+        turn = -1
+        action_display = "turn left"
+    elif steer_correction_radians > math.radians(10):
+        # If the target is more than 10 degrees left from the centre, steer right
+        turn = 1
+        action_display = "turn right"
+    else:
+        # If the target is less than 10 degrees from the centre, steer straight
+        turn = 0
+        action_display = "drive straight"
+    
+    self.controller_state.throttle = 1.0
+    self.controller_state.steer = turn
+    return action_display
+
+def find_correction(current: Vec3, ideal: Vec3, yaw) -> float:
+    # Finds the angle from current to ideal vector in the xy-plane. Angle will be between -pi and +pi.
+    angle_between_bot_and_target = math.atan2(ideal.y - current.y, ideal.x - current.x)
+
+    diff = angle_between_bot_and_target - yaw
+
+    # Make sure that diff is between -pi and +pi.
+    if abs(diff) > math.pi:
+        if diff < 0:
+            diff += 2 * math.pi
+        else:
+            diff -= 2 * math.pi
+
+    return diff
+
+def turning_radius(car_speed) -> float:  # Thx Dom
+    radius = 156 + 0.1 * car_speed + 0.000069 * car_speed ** 2 + 0.000000164 * car_speed ** 3 - 5.62e-11 * car_speed ** 4
+    return radius
+
+def inside_turning_radius(my_car, car_to_desired_location) -> bool:
+
+    rotation_matrix = Matrix3D(
+    [
+        my_car.physics.rotation.pitch,
+        my_car.physics.rotation.yaw,
+        my_car.physics.rotation.roll,
+    ])
+    local = rotation_matrix.dot(car_to_desired_location)
+    local = Vec3(local)
+    car_speed = my_car.physics.velocity
+    car_speed = Vec3(car_speed).length()
+    turn_radius = turning_radius(car_speed)
+    return turn_radius > min((local - Vec3(0, -turn_radius, 0)).length(), (local - Vec3(0, turn_radius, 0)).length())
+
+# Jumping (And eventually, hopefully, air movement)
+
+def jump(self):
+    self.controller_state.jump = True
+
+# Braking
+def brake_by(self, my_car, location):
+     # Make car break
+    self.controller_state.boost = False
+    self.controller_state.hand_brake = True
+    self.controller_state.throttle = 0
+
+def brake_and_orient(self, my_car, goal, ball):
+    
+    # Make car break
+    self.controller_state.boost = False
+    self.controller_state.hand_brake = True
+    self.controller_state.throttle = 0.0
+
+    # Calculate car reorientation
+    car_orientation = Orientation(my_car.physics.rotation)
+    car_direction = car_orientation.forward
+    car_yaw = my_car.physics.rotation.yaw
+    ball_future_location = get_ball_predicted_pos(self, 30)
+    car_to_ball = ball_future_location - my_car.physics.location
+    steer_correction_radians = find_correction(car_direction, car_to_ball, car_yaw)
+
+    if my_car.physics.velocity.x <= 30.0 and my_car.physics.velocity.y <= 30.0:
+        if steer_correction_radians < math.radians(-10):
+            # If the target is more than 10 degrees right from the centre, steer left
+            turn = -1
+        elif steer_correction_radians > math.radians(10):
+            # If the target is more than 10 degrees left from the centre, steer right
+            turn = 1
+        else:
+            # If the target is less than 10 degrees from the centre, steer straight
+            turn = 0
+
+        self.controller_state.hand_brake = False
+        self.controller_state.throttle = 1.0
+        self.controller_state.steer = turn
+    
+# Defense
+def defend_goal(self, my_car, goal, ball) -> str:
+    car_location = Vec3(my_car.physics.location)
+    goal_location = Vec3(goal.location)
+    car_to_goal = goal_location - car_location
+
+    ball_future_location = get_ball_predicted_pos(self, 30)
+    car_to_ball = ball_future_location - my_car.physics.location
+    action_display = "defending"
+    # checks if car is at goal location
+    car_speed = Vec3(my_car.physics.velocity).length()
+    print(car_speed)
+    if car_to_goal.length() > 1000:  
+        drive_toward(self, my_car, car_to_goal)
+        action_display = "driving to goal"
+    elif car_to_goal.length() < 1000 and car_speed > 600:
+        brake_and_orient(self, my_car, goal, ball)
+        # Car is at goal
+    else:
+        drive_toward(self, my_car, car_to_ball)
+
+    return action_display
+
+
+# Offense
---
+++ src/CarstianoRevnaldbot.py
@@ -0,0 +1,118 @@
+import math
+
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+
+from util.orientation import Orientation
+from util.vec import Vec3
+
+from Actions import *
+from Debug import *
+
+#USAGE: ALWAYS PUT OUR BOT ON LEFT SIDE OF RLBOT GUI
+#TODO Create check for future ball position and whether it's close to either goal
+#TODO Create check for distance between ball and car if ball is near opponents goal, then find proper distance
+#TODO Create methods to calculate where to drive to hit ball at proper angle for defense and offense
+#TODO Create action for jumping and hitting the ball at proper angle
+#TODO Method to check if the game is in the kickoff state
+
+
+class MyBot(BaseAgent):
+
+    def initialize_agent(self):
+        # This runs once before the bot starts up
+        self.controller_state = SimpleControllerState()
+        
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        ball_location = Vec3(packet.game_ball.physics.location)
+
+        my_car = packet.game_cars[self.index]
+        car_location = Vec3(my_car.physics.location)
+        car_to_ball = ball_location - car_location
+        car_to_ball_len = car_to_ball.length()
+
+        if car_to_ball_len > 1000:
+            ball_future_location = get_ball_predicted_pos(self, 30)
+            car_to_ball = ball_future_location - car_location
+
+        
+        info = self.get_field_info()
+        boost_location = Vec3(find_nearest_boost(info, car_location, packet))
+        car_to_boost = boost_location - car_location
+
+        # Determine which goal our bot is defending/attacking
+        ourGoalIndex = my_car.team
+        opponentGoalIndex = 0
+        if ourGoalIndex == 0:
+            opponentGoalIndex = 1
+        else:
+            opponentGoalIndex = 0
+    
+        # If Statments in order to implement behavior tree
+        #if kickoff --> drive at ball
+        
+        #GOAL 0 is defended by bots on right side of GUI (Bot's on left side score on goal 0)
+        opponentGoalDist = calculate_ball_to_goal(ball_location, info.goals[opponentGoalIndex])
+
+        #GOAL 1 is defended by bots on left side of GUI (Bot's on right side score on goal 1)
+        ourGoalDist = calculate_ball_to_goal(ball_location, info.goals[ourGoalIndex])
+
+        # Check if it is the kickoff
+        if is_kickoff(packet):
+            action_display = "kick off"
+            drive_for_kickoff(self, my_car, car_to_ball)
+
+        # Check if ball is near our goal
+        elif ourGoalDist < 4000:
+            #Drive to defensive position
+            action_display = defend_goal(self, my_car, info.goals[ourGoalIndex], packet.game_ball)
+
+
+        # Check if ball is near opponent's goal
+        elif opponentGoalDist < 4000:
+            action_display = "offensive"
+            ball_future_location = get_ball_predicted_pos(self, 40)
+            car_to_ball = ball_future_location - car_location
+            #Drive to offensive position
+            drive_toward_ball(self, my_car, car_to_ball)
+            
+
+        # Check if boost level is low
+        elif get_car_boost_level(my_car) < 35:
+            action_display = "getting boost"  
+            drive_toward_boost(self, my_car, car_to_boost)
+
+        # Default    
+        else:
+            action_display = "just drivin"
+            #TODO Figure out how to properly time boosts
+            #Current boost activation is in defend_goal
+            self.controller_state.boost = False
+            drive_toward_ball(self, my_car, car_to_ball)
+    
+        draw_debug_message(self.renderer, my_car, action_display)
+
+        return self.controller_state
+
+def is_kickoff(packet) -> bool:
+    return packet.game_info.is_kickoff_pause
+
+def find_nearest_boost(info, car_location, packet) -> Vec3:
+    closest_pad_index = 0
+    closest_pad_distance = 100000
+    for i in range(info.num_boosts):  # For loop to get location of all pads
+        pad = info.boost_pads[i]
+        # To get Vect3 coordinate of pad
+        padLoc = pad.location
+        new_value = car_location.dist(padLoc)
+        if new_value < closest_pad_distance:
+            if packet.game_boosts[i].is_active:
+                closest_pad_distance = new_value
+                closest_pad_index = i
+
+    return info.boost_pads[closest_pad_index].location
+
+
+    
+    
+
