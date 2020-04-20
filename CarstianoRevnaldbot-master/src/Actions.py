c43e77fa7487fef7d6fc5d59dd851482b87bfb84
Angel Rodriguez <34219974+rodriang000@users.noreply.github.com>
2020-04-20 16:08:20 -0400

    O-fence, like a fence that also attacks you.

--- CarstianoRevnaldbot-master/src/Actions.py
+++ CarstianoRevnaldbot-master/src/Actions.py
@@ -9,6 +9,7 @@ from util.vec import Vec3
 from Debug import *
 from Calculations import *
 from util.Matrix import *
+from util import *
 
 
 # TODO calculate turning radius and turning radius needed, if you can't make the turn then brake
@@ -18,14 +19,14 @@ def get_ball_predicted_pos(self, sliceNum) -> Vec3:
     ball_prediction = self.get_ball_prediction_struct()
     return Vec3(ball_prediction.slices[sliceNum].physics.location)
 
-   
+
 # General Driving
-def drive_for_kickoff(self, my_car, car_to_desired_location) -> str:
+def drive_for_kickoff(self, target) -> str:
     # Find the direction of our car using the Orientation class
-    car_orientation = Orientation(my_car.physics.rotation)
+    car_orientation = Orientation(self.bot_rot)
     car_direction = car_orientation.forward
-    car_yaw = my_car.physics.rotation.yaw
-    steer_correction_radians = find_correction(car_direction, car_to_desired_location, car_yaw)
+    car_yaw = car_orientation.yaw
+    steer_correction_radians = find_correction(car_direction, target, car_yaw)
 
     if steer_correction_radians < math.radians(-10):
         # If the target is more than 10 degrees right from the centre, steer left
@@ -39,19 +40,16 @@ def drive_for_kickoff(self, my_car, car_to_desired_location) -> str:
         # If the target is less than 10 degrees from the centre, steer straight
         turn = 0
         action_display = "drive straight"
-        
+
     self.controller_state.throttle = 1.0
-    self.controller_state.steer = turn 
+    self.controller_state.steer = turn
     self.controller_state.boost = True
 
-    car_location = Vec3(my_car.physics.location)
-    if (car_location.dist(car_to_desired_location) < 900):
+    car_location = self.bot_loc
+    if (car_location.dist(target) < 900):
         jump(self)
 
-    return action_display  
-
-def drive_toward_ball(self, my_car, car_to_ball):
-    drive_toward(self, my_car, car_to_ball)
+    return action_display
     
 def calculate_ball_to_goal(ball_location, goal)-> float:
     return ball_location.dist(goal.location)
@@ -59,40 +57,34 @@ def calculate_ball_to_goal(ball_location, goal)-> float:
 def get_car_boost_level(my_car) -> float:
     return my_car.boost
 
-def drive_toward_boost(self, my_car, car_to_boost):
-    drive_toward(self, my_car, car_to_boost)
 
-def drive_toward(self, my_car, car_to_desired_location) -> str:
+def drive_toward(self, target):
     self.controller_state.boost = False
      # Find the direction of our car using the Orientation class
-    car_orientation = Orientation(my_car.physics.rotation)
+    car_orientation = Orientation(self.bot_rot)
     car_direction = car_orientation.forward
-    car_yaw = my_car.physics.rotation.yaw
-    steer_correction_radians = find_correction(car_direction, car_to_desired_location, car_yaw)
+    car_yaw = car_orientation.yaw
+    steer_correction_radians = find_correction(car_direction, target, car_yaw)
+
 
     if steer_correction_radians < math.radians(-10):
         # If the target is more than 10 degrees right from the centre, steer left
         turn = -1
-        action_display = "turn left"
     elif steer_correction_radians > math.radians(10):
         # If the target is more than 10 degrees left from the centre, steer right
         turn = 1
-        action_display = "turn right"
     else:
         # If the target is less than 10 degrees from the centre, steer straight
         turn = 0
-        action_display = "drive straight"
+
     
     self.controller_state.throttle = 1.0
     self.controller_state.steer = turn
-    return action_display
 
 def find_correction(current: Vec3, ideal: Vec3, yaw) -> float:
     # Finds the angle from current to ideal vector in the xy-plane. Angle will be between -pi and +pi.
     angle_between_bot_and_target = math.atan2(ideal.y - current.y, ideal.x - current.x)
-
     diff = angle_between_bot_and_target - yaw
-
     # Make sure that diff is between -pi and +pi.
     if abs(diff) > math.pi:
         if diff < 0:
@@ -123,8 +115,23 @@ def inside_turning_radius(my_car, car_to_desired_location) -> bool:
 
 # Jumping (And eventually, hopefully, air movement)
 
-def jump(self):
-    self.controller_state.jump = True
+def jump(self, pitch=-1, roll=.05, jump_timeout=0.1, flick_timeout=0.15):
+    if self.flick_time == 0 and self.bot_ground:
+        self.controller_state.jump = True
+        self.jump_time_end = self.game_time + jump_timeout
+        self.flick_time = self.game_time + flick_timeout
+        if pitch > 1:
+            pitch = 1
+        elif pitch < -1:
+            pitch = -1
+        self.controller_state.pitch = pitch
+        roll = roll * 1.3
+        if roll > 1:
+            roll = 1
+        elif roll < -1:
+            roll = -1
+        self.controller_state.roll = roll
+
 
 # Braking
 def brake_by(self, my_car, location):
@@ -165,6 +172,7 @@ def brake_and_orient(self, my_car, goal, ball):
     
 # Defense
 def defend_goal(self, my_car, goal, ball) -> str:
+    block_shot(self, my_car, goal, ball)
     car_location = Vec3(my_car.physics.location)
     goal_location = Vec3(goal.location)
     car_to_goal = goal_location - car_location
@@ -174,12 +182,14 @@ def defend_goal(self, my_car, goal, ball) -> str:
     action_display = "defending"
     # checks if car is at goal location
     car_speed = Vec3(my_car.physics.velocity).length()
-    print(car_speed)
+
     if car_to_goal.length() > 1000:  
         drive_toward(self, my_car, car_to_goal)
         action_display = "driving to goal"
     elif car_to_goal.length() < 1000 and car_speed > 600:
         brake_and_orient(self, my_car, goal, ball)
+        block_shot(self, my_car, goal, ball)
+        action_display = "blocking shot"
         # Car is at goal
     else:
         drive_toward(self, my_car, car_to_ball)
@@ -187,4 +197,149 @@ def defend_goal(self, my_car, goal, ball) -> str:
     return action_display
 
 
+def block_shot(self, my_car, goal, ball):
+   
+    # Calculate car reorientation
+    car_orientation = Orientation(my_car.physics.rotation)
+    car_direction = car_orientation.forward
+    goal_direction = Vec3(goal.direction)
+
+    angle_to_goal = car_direction.ang_to(goal_direction)
+    
+    
+    ball_future_location = get_ball_predicted_pos(self, 30)
+    my_car_location = Vec3(my_car.physics.location)
+    car_to_ball = my_car_location.dist(ball_future_location)
+
+    if angle_to_goal < math.pi/2 and angle_to_goal > 0:
+        # print("< pi/2 and > 0 jumpin left")
+        self.controller_state.jump = True
+        self.controller_state.steer = -1
+    elif angle_to_goal > math.pi/2 and angle_to_goal < math.pi:
+        # print("< pi and > pi/2 jumpin right")
+        self.controller_state.jump = True
+        self.controller_state.steer = 1
+    else:
+        self.controller_state.jump = False
+    if car_to_ball < 100:
+        # print("less than 100")
+        self.controller_state.jump = True
+    else:
+        self.controller_state.jump = False
+    #if car forward vector is parallel to goal, fine
+    #if car forward vector is > pi/2 & < pi go to pi (believe this means we are left of goal)
+    #if car forward vector is < pi/2 & > 0 go to 0 (believe this means we are right of goal)
+
 # Offense
+def play_offense(self) -> Vec3:
+    # Information needed
+    self.renderer.begin_rendering()
+
+    # Information needed
+    # 1.) The predicted ball path
+    ball_prediction = self.get_ball_prediction_struct()
+    if ball_prediction is not None:
+        prediction_slices = ball_prediction.slices[0:120]
+        locations = []
+        for i in range(0, len(prediction_slices)):
+            locations.append(prediction_slices[i].physics.location)
+        self.renderer.draw_polyline_3d(locations, self.renderer.red())
+
+    center = Vec3(0, 5150 * -sign(self.team), 200)
+
+    timeIndex = determineTarget(self)
+    target = Vec3(locations[timeIndex])
+    goal = target - self.bot_loc
+
+    self.renderer.draw_line_3d(self.ball_loc, center, self.renderer.green())
+    self.renderer.draw_line_3d(self.bot_loc,  target, self.renderer.black())
+
+    future_list = []
+    for i in range(0, 120):
+        tempTarget = Vec3(locations[i])
+        temppt1 = findAngle(tempTarget, center)
+        future_list.append(temppt1)
+
+    total_dif = 0
+    for i in range(1, 120):
+        prevVal = future_list[i - 1]
+        curVal = future_list[i]
+        if prevVal > curVal:
+            total_dif += 1
+        else:
+            total_dif -= 1
+
+    ballY = self.ball_loc.y
+    carY = self.bot_loc.y
+    behind = False
+    if abs(center.y - ballY) < abs(center.y - carY):  # Car is behind the ball
+        behind = True
+
+    if total_dif > 70: # The future location and ball are generally moving in the same direction
+        car_location = self.bot_loc
+        ballX = self.ball_loc.x
+        carX = self.bot_loc.x
+
+        if (ballX > carX + 10): #Ball is further right than the car, steer right
+            self.controller_state.steer = 1
+        elif(ballX - 10 < carX):
+            self.controller_state.steer = -1
+
+        if car_location.dist(target) < 400 and self.ball_loc.z <= 120 and future_list[timeIndex < 5] and behind == True:
+            #Ball is to the left
+            jump(self)
+
+    print(Vec3(locations[119]).dist(center))
+    if behind == True or Vec3(locations[119]).dist(center) <= 2400:
+        drive_toward(self, goal)
+    else:
+        target = Vec3(locations[60])
+        goal = target - self.bot_loc
+        self.renderer.draw_line_3d(self.bot_loc, target, self.renderer.white())
+        drive_toward(self, goal)
+    # drive_toward(self, goal)
+
+    self.renderer.end_rendering()
+    return goal
+
+def sign(x):
+    if x <= 0:
+        return -1
+    else:
+        return 1
+
+def determineTarget(self) -> int:
+    oldValue = Vec3.dist(self.ball_loc, self.bot_loc) #Scales between 0 and 120 seconds
+    oldMin = 200
+    oldMax = 8000
+    newMax = 120
+    newMin = 0
+    oldRange = oldMax - oldMin
+    newRange = newMax - newMin
+    target = int((((oldValue - oldMin) * newRange) // oldRange) + newMin)
+    return(target)
+
+def relative_location(center: Vec3, ori: Orientation, target: Vec3) -> Vec3:
+    """
+    Returns target as a relative location from center's point of view, using the given orientation. The components of
+    the returned vector describes:
+
+    * x: how far in front
+    * y: how far right
+    * z: how far above
+    """
+    x = (target - center).dot(ori.forward)
+    y = (target - center).dot(ori.right)
+    z = (target - center).dot(ori.up)
+    return Vec3(x, y, z)
+
+def findAngle(vec1: Vec3, vec2: Vec3) -> float:
+    x1 = vec1.x
+    y1 = vec1.y
+    x2 = vec2.x
+    y2 = vec2.y
+    top_section = (x1 * x2) + (y1 * y2)
+    bottom_section = math.sqrt(x1**2 + y1**2) * math.sqrt(x2**2 + y2**2)
+    if bottom_section == 0:
+        return 0
+    return math.acos(top_section/bottom_section)
--- CarstianoRevnaldbot-master/src/CarstianoRevnaldbot.py
+++ CarstianoRevnaldbot-master/src/CarstianoRevnaldbot.py
@@ -9,21 +9,200 @@ from util.vec import Vec3
 from Actions import *
 from Debug import *
 
-#USAGE: ALWAYS PUT OUR BOT ON LEFT SIDE OF RLBOT GUI
-#TODO Create check for future ball position and whether it's close to either goal
-#TODO Create check for distance between ball and car if ball is near opponents goal, then find proper distance
-#TODO Create methods to calculate where to drive to hit ball at proper angle for defense and offense
-#TODO Create action for jumping and hitting the ball at proper angle
-#TODO Method to check if the game is in the kickoff state
-
 
 class MyBot(BaseAgent):
 
     def initialize_agent(self):
         # This runs once before the bot starts up
         self.controller_state = SimpleControllerState()
+        self.num_cars = 0
+        # Controller inputs
+        self.controller_state.throttle = 0
+        self.controller_state.steer = 0
+        self.controller_state.pitch = 0
+        self.controller_state.yaw = 0
+        self.controller_state.roll = 0
+        self.controller_state.boost = False
+        self.controller_state.jump = False
+        self.controller_state.handbrake = False
+        # Game values
+        self.bot_loc_x = 0
+        self.bot_loc_y = 0
+        self.bot_loc_z = 0
+        self.bot_speed_x = 0
+        self.bot_speed_y = 0
+        self.bot_speed_z = 0
+        self.bot_rot_yaw = 0
+        self.bot_rot_roll = 0
+        self.bot_rot_pitch = 0
+        self.bot_jumped = False
+        self.bot_doublejumped = False
+        self.bot_ground = False
+        self.bot_sonic = False
+        self.bot_dodge = False
+        self.bot_boost = 0
+        self.bot_max_speed = [1410, 2300]  # normal, boost
+        self.bot_boost_to_max_speed = [27, 57, 30]  # to normal, to boost, from normal to boost
+        self.ball_loc_x = 0
+        self.ball_loc_y = 0
+        self.ball_loc_z = 0
+        self.ball_speed_x = 0
+        self.ball_speed_y = 0
+        self.ball_speed_z = 0
+        self.ball_ang_speed_x = 0
+        self.ball_ang_speed_y = 0
+        self.ball_ang_speed_z = 0
+        self.ball_lt_x = 0
+        self.ball_lt_y = 0
+        self.ball_lt_z = 0
+        self.ball_lt_time = 0
+        self.game_time = 0
+        self.game_ball_touched = False
+        # game values converted
+        self.bot_yaw = 0
+        self.bot_pitch = 0
+        self.bot_roll = 0
+        self.angle_front_to_target = 0
+        self.angle_car_ball = 0
+        # custom values
+        self.angle_car_ball = 0
+        self.distance_car_ball = 0
+        self.bot_speed_linear = 0
+        self.ball_initial_speed_z = 0
+        self.ball_initial_pos_z = 0
+        self.ball_test_for_start = False
+
+        self.ttt = 0.00
+        self.lt_time = 0.00
+        self.prediction = [[], []]
+        self.printed = False
+
+        self.predicted_loc = [[0, 0, 0], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
+        self.game_time_lst = []
+        self.ball_speed_x_lst = []
+        self.ball_speed_y_lst = []
+        self.ball_speed_z_lst = []
+        self.ball_ang_speed_x_lst = []
+        self.ball_ang_speed_y_lst = []
+        self.ball_ang_speed_z_lst = []
+        self.ball_loc_x_lst = []
+        self.ball_loc_y_lst = []
+        self.ball_loc_z_lst = []
+
+        self.bounce_n = 0
+        self.hit = False
+        self.realtime = [[], []]
+        self.time_to_hit = 1
+        self.jump_time_end = 0
+        self.flick_time = 0
+        self.stop_flick = False
+        self.last_predict = 0
+
+    def distance(self, x1, y1, z1, x2, y2, z2):
+        distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+        return distance
+
+    def get_values(self, values):
+        self.controller_state.boost = False
+
+        if self.jump_time_end < self.game_time:
+            self.controller_state.jump = False
+
+        if self.stop_flick:
+            self.controller_state.pitch = 0
+            self.controller_state.roll = 0
+            self.flick_time = 0
+            self.stop_flick = False
+            # print('flip stopped')
+
+        if self.flick_time < self.game_time and self.flick_time != 0:
+            self.controller_state.jump = True
+            self.stop_flick = True
+            # print('stopping flick')
+
+        # Update game data variables
+        self.bot_team = values.game_cars[self.index].team
+
+        self.bot_loc = Vec3(values.game_cars[self.index].physics.location)
+        self.bot_rot = values.game_cars[self.index].physics.rotation
+        self.ball_loc = Vec3(values.game_ball.physics.location)
+        self.ball_lt = values.game_ball.latest_touch
+        self.bot_yaw = values.game_cars[self.index].physics.rotation.yaw
+        # get game values
+        self.bot_loc_x = values.game_cars[self.index].physics.location.x
+        self.bot_loc_y = values.game_cars[self.index].physics.location.y
+        self.bot_loc_z = values.game_cars[self.index].physics.location.z
+        self.bot_speed_x = values.game_cars[self.index].physics.velocity.x
+        self.bot_speed_y = values.game_cars[self.index].physics.velocity.y
+        self.bot_speed_z = values.game_cars[self.index].physics.velocity.z
+        self.bot_jumped = values.game_cars[self.index].jumped
+        self.bot_doublejumped = values.game_cars[self.index].double_jumped
+        self.bot_sonic = values.game_cars[self.index].is_super_sonic
+        self.bot_ground = values.game_cars[self.index].has_wheel_contact
+        self.bot_boost = values.game_cars[self.index].boost
+
+        self.ball_loc_x = values.game_ball.physics.location.x
+        self.ball_loc_x_lst += [self.ball_loc_x]
+        self.ball_loc_y = values.game_ball.physics.location.y
+        self.ball_loc_y_lst += [self.ball_loc_y]
+        self.ball_loc_z = values.game_ball.physics.location.z
+        self.ball_loc_z_lst += [self.ball_loc_z]
+        self.ball_speed_x = values.game_ball.physics.velocity.x
+        self.ball_speed_x_lst += [self.ball_speed_x]
+        self.ball_speed_y = values.game_ball.physics.velocity.y
+        self.ball_speed_y_lst += [self.ball_speed_y]
+        self.ball_speed_z = values.game_ball.physics.velocity.z
+        self.ball_speed_z_lst += [self.ball_speed_z]
+        self.ball_ang_speed_x = values.game_ball.physics.angular_velocity.x
+        self.ball_ang_speed_x_lst += [self.ball_ang_speed_x]
+        self.ball_ang_speed_y = values.game_ball.physics.angular_velocity.y
+        self.ball_ang_speed_y_lst += [self.ball_ang_speed_y]
+        self.ball_ang_speed_z = values.game_ball.physics.angular_velocity.z
+        self.ball_ang_speed_z_lst += [self.ball_ang_speed_z]
+        self.ball_lt_x = values.game_ball.latest_touch.hit_location.x
+        self.ball_lt_y = values.game_ball.latest_touch.hit_location.y
+        self.ball_lt_z = values.game_ball.latest_touch.hit_location.z
+        self.ball_lt_time = values.game_ball.latest_touch.time_seconds
+        self.ball_lt_normal_x = values.game_ball.latest_touch.hit_normal.x
+        self.ball_lt_normal_y = values.game_ball.latest_touch.hit_normal.y
+        self.ball_lt_normal_z = values.game_ball.latest_touch.hit_normal.z
+
+        self.game_time = values.game_info.seconds_elapsed
+        self.game_time_lst += [self.game_time]
+        self.game_time_left = values.game_info.game_time_remaining
+        self.game_overtime = values.game_info.is_overtime
+        self.game_active = values.game_info.is_round_active
+        self.game_ended = values.game_info.is_match_ended
+        self.renderCalls = []
+        # self.game_ball_touched = values.game_info.is_kickoff_pause
+        if values.game_info.is_kickoff_pause:
+            self.game_ball_touched = False
+        else:
+            self.game_ball_touched = True
+
+        self.num_cars = values.num_cars
+
+        # get custom values
+        self.realime = [self.game_time_lst, [[self.ball_loc_x_lst], [self.ball_loc_y_lst], [self.ball_loc_z_lst]]]
+        self.angle_car_ball = math.degrees(
+            math.atan2(self.ball_loc_y - self.bot_loc.y, self.ball_loc_x - self.bot_loc.x)) - self.bot_rot_yaw
+        self.distance_car_ball = self.distance(self.bot_loc_x, self.bot_loc_y, self.bot_loc_z, self.ball_loc_x,
+                                               self.ball_loc_y, self.ball_loc_z) - 93
+        self.bot_speed_linear = self.distance(self.bot_speed_x, self.bot_speed_y, self.bot_speed_z, 0, 0, 0)
+
+        # Convert car's yaw, pitch and roll and convert from Unreal Rotator units to degrees
+        self.bot_rot_yaw = abs(self.bot_rot.yaw) / (2 * math.pi) * 360
+        if self.bot_rot.yaw < 0:
+            self.bot_rot_yaw *= -1
+        self.bot_rot_pitch = abs(self.bot_rot.pitch) / (2 * math.pi) * 360
+        if self.bot_rot.pitch < 0:
+            self.bot_rot_pitch *= -1
+        self.bot_rot_roll = abs(self.bot_rot.roll) / (2 * math.pi) * 360
+        if self.bot_rot.roll < 0:
+            self.bot_rot_roll *= -1
         
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        self.get_values(packet) #Update agent information
         ball_location = Vec3(packet.game_ball.physics.location)
 
         my_car = packet.game_cars[self.index]
@@ -42,7 +221,6 @@ class MyBot(BaseAgent):
 
         # Determine which goal our bot is defending/attacking
         ourGoalIndex = my_car.team
-        opponentGoalIndex = 0
         if ourGoalIndex == 0:
             opponentGoalIndex = 1
         else:
@@ -60,38 +238,42 @@ class MyBot(BaseAgent):
         # Check if it is the kickoff
         if is_kickoff(packet):
             action_display = "kick off"
-            drive_for_kickoff(self, my_car, car_to_ball)
+            drive_for_kickoff(self,  car_to_ball)
 
         # Check if ball is near our goal
-        elif ourGoalDist < 4000:
-            #Drive to defensive position
-            action_display = defend_goal(self, my_car, info.goals[ourGoalIndex], packet.game_ball)
+        # elif ourGoalDist < 4000:
+        #     #Drive to defensive position
+        #     action_display = defend_goal(self, my_car, info.goals[ourGoalIndex], packet.game_ball)
 
 
         # Check if ball is near opponent's goal
-        elif opponentGoalDist < 4000:
+        elif opponentGoalDist < 5000:
             action_display = "offensive"
-            ball_future_location = get_ball_predicted_pos(self, 40)
-            car_to_ball = ball_future_location - car_location
-            #Drive to offensive position
-            drive_toward_ball(self, my_car, car_to_ball)
-            
 
-        # Check if boost level is low
-        elif get_car_boost_level(my_car) < 35:
-            action_display = "getting boost"  
-            drive_toward_boost(self, my_car, car_to_boost)
+        # # Check if boost level is low
+        # elif get_car_boost_level(my_car) < 35:
+        #     action_display = "getting boost"
+        #     drive_toward_boost(self, my_car, car_to_boost)
+
 
         # Default    
         else:
-            action_display = "just drivin"
-            #TODO Figure out how to properly time boosts
+            action_display = "offensive"
             #Current boost activation is in defend_goal
             self.controller_state.boost = False
-            drive_toward_ball(self, my_car, car_to_ball)
-    
-        draw_debug_message(self.renderer, my_car, action_display)
 
+        draw_debug_message(self.renderer, my_car, action_display)
+        if(action_display == "blocking shot"):
+            draw_block_debug(self.renderer, my_car, info.goals[ourGoalIndex],  packet.game_ball)
+        if(action_display == "offensive"):
+            play_offense(self)
+
+
+
+        if(action_display == "getting boost"):
+            self.renderer.begin_rendering()
+            self.renderer.draw_line_3d(self.bot_loc, car_to_boost, self.renderer.yellow())
+            self.renderer.end_rendering()
         return self.controller_state
 
 def is_kickoff(packet) -> bool:
@@ -113,6 +295,6 @@ def find_nearest_boost(info, car_location, packet) -> Vec3:
     return info.boost_pads[closest_pad_index].location
 
 
-    
-    
+
+
 
